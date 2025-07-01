f90d5d8104425a6e01d1181936867dc047884608
Mikhail Fam <77157663+mmcmNew@users.noreply.github.com>
2025-07-01 20:36:00 +0500

    Enable CSRF protection

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,12 +7,15 @@ from .config import WorkConfig, TestingConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_wtf import CSRFProtect
+from flask_wtf.csrf import generate_csrf
 # from app.socketio_instance import socketio
 from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 migrate = Migrate(render_as_batch=True)
 socketio = SocketIO(logger=False, engineio_logger=False, cors_allowed_origins="*", async_mode='eventlet', debug=False)
+csrf = CSRFProtect()
 
 
 def create_app(config_type='work'):
@@ -28,6 +31,8 @@ def create_app(config_type='work'):
         static_folder=dist_folder,
         static_url_path=''  # Статика будет доступна по /
     )
+
+    csrf.init_app(app)
 
     app.config['DIST_FOLDER'] = dist_folder
 
@@ -45,6 +50,12 @@ def create_app(config_type='work'):
     db.init_app(app)
     migrate.init_app(app, db)
     socketio.init_app(app)
+
+    @app.after_request
+    def set_csrf_cookie(response):
+        response.set_cookie('csrf_token', generate_csrf())
+        return response
+
     with app.app_context():
 
         from .main import main as main_blueprint
