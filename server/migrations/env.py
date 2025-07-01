3df6b4918ca5f3915a98718b848c687ec71e4ed5
Mikhail Fam <77157663+mmcmNew@users.noreply.github.com>
2025-07-01 20:35:03 +0500

    Add unique constraints for User and Alembic migration

---
+++ server/migrations/env.py
@@ -0,0 +1,56 @@
+from __future__ import with_statement
+import logging
+from logging.config import fileConfig
+
+from alembic import context
+from flask import current_app
+
+from app import create_app, db
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+logger = logging.getLogger('alembic.env')
+
+app = create_app('test')
+app.app_context().push()
+
+target_metadata = db.metadata
+
+
+def run_migrations_offline():
+    url = app.config.get('SQLALCHEMY_DATABASE_URI')
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        render_as_batch=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = db.engine
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
