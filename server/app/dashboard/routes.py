cf3534cbc2e980bda3b3418396fb0b5c884f9224
Mikhail Fam <77157663+mmcmNew@users.noreply.github.com>
2025-07-03 17:22:19 +0500

    Fix per-user dashboard handling

--- server/app/dashboard/routes.py
+++ server/app/dashboard/routes.py
@@ -26,16 +26,20 @@ def update_dashboard():
     # current_app.logger.info(f'update_dashboard: {dashboard_id}, {name}, {theme_mode}, {calendar_settings}')
 
     dashboard_db = Dashboard.query.filter_by(id=dashboard_id, user_id=current_user.id).first()
-    print(dashboard_db)
-    if not dashboard_db:
-        dashboard_db = Dashboard(id=dashboard_id, user_id=current_user.id, name=name, containers=containers, theme_mode=theme_mode,
-                                 calendar_settings=calendar_settings_json)
+    if dashboard_id == 0 or not dashboard_db:
+        dashboard_db = Dashboard(user_id=current_user.id, name=name, containers=containers,
+                                 theme_mode=theme_mode, calendar_settings=calendar_settings_json)
+        db.session.add(dashboard_db)
+        db.session.commit()
+        dashboard_id = dashboard_db.id
     else:
         dashboard_db.name = name
         dashboard_db.containers = containers
         dashboard_db.theme_mode = theme_mode
         dashboard_db.calendar_settings = calendar_settings_json
 
+    current_user.last_dashboard_id = dashboard_id
+    db.session.add(current_user)
     db.session.add(dashboard_db)
     db.session.commit()
 
@@ -49,6 +53,24 @@ def get_dashboard(dashboard_id):
     # print(f'get_dashboard: {dashboard_db.to_dict()}')
     if not dashboard_db:
         return jsonify({"error": "Dashboard not found"}), 404
+    return jsonify(dashboard_db.to_dict())
+
+
+@dashboard.route('/dashboard/last', methods=['GET'])
+@login_required
+def get_last_dashboard():
+    dashboard_db = None
+    if current_user.last_dashboard_id:
+        dashboard_db = Dashboard.query.filter_by(id=current_user.last_dashboard_id, user_id=current_user.id).first()
+
+    if not dashboard_db:
+        dashboard_db = Dashboard(user_id=current_user.id, name="dashboard", containers=[], theme_mode='light')
+        db.session.add(dashboard_db)
+        db.session.commit()
+        current_user.last_dashboard_id = dashboard_db.id
+        db.session.add(current_user)
+        db.session.commit()
+
     return jsonify(dashboard_db.to_dict())
 
 
